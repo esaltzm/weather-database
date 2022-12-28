0f87a869ab14d0ebb4034dd3229bb27fa0eb7966
esaltzm <elisaltzman@gmail.com>
2022-12-28 11:05:57 -0500

    inspect file to get variable names

--- grb2parser.py
+++ grb2parser.py
@@ -1,3 +1,7 @@
 import xarray as xr
+import matplotlib.pyplot as plt
 
 ds = xr.open_dataset('./grb2_files/rap_130_20221220_0000_001.grb2', engine="cfgrib", filter_by_keys={'stepType': 'instant', 'typeOfLevel': 'surface'})
+
+for item in ds:
+    print(f'{item}, {ds[item].attrs["long_name"]}, {ds[item].attrs["units"]}')
