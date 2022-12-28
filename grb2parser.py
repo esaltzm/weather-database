05d578a86f2db74ff1706a535bba8b757db297f8
esaltzm <elisaltzman@gmail.com>
2022-12-28 11:09:24 -0500

    xarray to pandas df, read df

--- grb2parser.py
+++ grb2parser.py
@@ -6,6 +6,13 @@ ds = xr.open_dataset('./grb2_files/rap_130_20221220_0000_001.grb2', engine="cfgr
 # for item in ds:
 #     print(f'{item}, {ds[item].attrs["long_name"]}, {ds[item].attrs["units"]}')
 
-plt.contourf(ds['t'])
-plt.colorbar()
-plt.show()
+# plt.contourf(ds['t'])
+# plt.colorbar()
+# plt.show()
+
+ds = ds.get('t')
+df = ds.to_dataframe()
+print(type(df))
+print(df.columns)
+for item in list(df.values)[:3]:
+    print(item)
