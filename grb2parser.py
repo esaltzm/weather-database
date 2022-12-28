0e9ab8961b5322f9598a02af13656a4e33b3576c
esaltzm <elisaltzman@gmail.com>
2022-12-28 11:07:45 -0500

    sanity check - plot temp data

--- grb2parser.py
+++ grb2parser.py
@@ -3,5 +3,9 @@ import matplotlib.pyplot as plt
 
 ds = xr.open_dataset('./grb2_files/rap_130_20221220_0000_001.grb2', engine="cfgrib", filter_by_keys={'stepType': 'instant', 'typeOfLevel': 'surface'})
 
-for item in ds:
-    print(f'{item}, {ds[item].attrs["long_name"]}, {ds[item].attrs["units"]}')
+# for item in ds:
+#     print(f'{item}, {ds[item].attrs["long_name"]}, {ds[item].attrs["units"]}')
+
+plt.contourf(ds['t'])
+plt.colorbar()
+plt.show()
