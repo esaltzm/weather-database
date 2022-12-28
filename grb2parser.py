afabbd7badf6b43b6f36183f849f7ea70467b0cd
esaltzm <elisaltzman@gmail.com>
2022-12-28 12:13:33 -0500

    refactor var df creation

--- grb2parser.py
+++ grb2parser.py
@@ -10,48 +10,22 @@ ds = xr.open_dataset('./grb2_files/rap_130_20221220_0000_001.grb2', engine="cfgr
 # 't', 'vis', 'gust', 'sde', 'prate', 'crain', 'ltng'
 # temp, visibility, wind gust speed, snow depth, precipitation rate, categorical rain,
 
-ds_temp = ds.get('t')
-df_temp = ds_temp.to_dataframe()
+ds_t = ds.get('t')
+df_t = ds_t.to_dataframe()
 # print(df_temp.columns)
 shift_longitude = lambda lon: (lon - 360) if (lon > 180) else lon 
 convert_temp = lambda t: t - 273.15 # K to C
-df_temp['longitude'] = df_temp['longitude'].apply(shift_longitude)
-df_temp['t'] = df_temp['t'].apply(convert_temp)
+df_t['longitude'] = df_t['longitude'].apply(shift_longitude)
+df_t['t'] = df_t['t'].apply(convert_temp)
 # for item in list(df_temp.values)[:3]:
 #     print(item)
 
-ds_vis = ds.get('vis')
-df_vis = ds_vis.to_dataframe()
-# print(df_vis.columns)
-# for item in list(df_vis.values)[:3]:
-#     print(item)
+var_dfs = {'df_t': df_t}
 
-ds_gust = ds.get('gust')
-df_gust = ds_gust.to_dataframe()
-# print(df_gust.columns)
-# for item in list(df_gust.values)[:3]:
-#     print(item)
+for var in ['vis', 'gust', 'sde', 'prate', 'crain', 'ltng']:
+    ds_var = ds.get(var)
+    df_var = ds_var.to_dataframe()
+    var_dfs['df_' + var] = df_var
 
-ds_sde = ds.get('sde')
-df_sde = ds_sde.to_dataframe()
-# print(df_sde.columns)
-# for item in list(df_sde.values)[:3]:
-#     print(item)
+print(var_dfs)
 
-ds_prate = ds.get('prate')
-df_prate = ds_prate.to_dataframe()
-# print(df_prate.columns)
-# for item in list(df_prate.values)[:3]:
-#     print(item)
-
-ds_crain = ds.get('crain')
-df_crain = ds_crain.to_dataframe()
-# print(df_crain.columns)
-# for item in list(df_crain.values)[:3]:
-#     print(item)
-
-ds_ltng = ds.get('ltng')
-df_ltng = ds_ltng.to_dataframe()
-print(df_ltng.columns)
-for item in list(df_ltng.values)[:3]:
-    print(item)
