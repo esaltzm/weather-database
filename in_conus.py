fc3cf622773312c961f0e1a9f2a19e8b218abfcf
esaltzm <elisaltzman@gmail.com>
2022-12-28 19:32:38 -0500

    filter states to only conus

--- in_conus.py
+++ in_conus.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 from shapely.geometry import Point, Polygon
 
 def get_borders():
-
     sf = shapefile.Reader('./state_data/cb_2015_us_state_20m-polygon')
     shapes = sf.shapes()
     records = sf.records()
     state_polygons = {}
     for i, record in enumerate(records):
-        state = record[5]
-        points = shapes[i].points
-        poly = Polygon(points)
-        state_polygons[state] = poly
+        state = record[len(record) - 1]
+        if state not in ['Alaska', 'Hawaii', 'Puerto Rico']:
+            points = shapes[i].points
+            poly = Polygon(points)
+            state_polygons[state] = poly
     return state_polygons
 
 state_polygons = get_borders()   
@@ -29,7 +29,3 @@ def in_us(lat, lon):
         if state.contains(p):
             return True
     return False
-
-plot_borders()
-plt.show()
-
